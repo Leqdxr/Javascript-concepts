5fc056667eabe37839005076ae5b87ab762d8db2
Aagaman Koirala <aagamankoirala10@gmail.com>
2026-08-18 22:04:42 +0545

    Complete writing notes on functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -50,3 +50,54 @@ function greetUser(username) {
 
 console.log(greetUser("Aagaman")); // This will print Hello Aagaman, welcome to the app
 console.log(greetUser()); // Return with no value becomes undefined, so we get "Your username can NOT be empty" "undefined"
+
+
+// Let's say you are working on a project which requires you to calculate sum of all prices in a cart
+// We don't know how many items a user may add to cart, we won't know how many values will be passed as arguments
+// In that case, we can use rest operator (...)
+// rest operator and spread operator(...) both use the same syntax, but wheather it is rest or spread depends on context, where it is used
+
+function calculateCartItems(...price) { // We can now accept more than 1 arguments because of this parameter
+    console.log(price);
+}
+
+calculateCartItems(200,300,400,500,600);
+// [ 200, 300, 400, 500, 600 ]
+
+function specialCase(num1,num2,...price) {
+    console.log(price);
+}
+
+specialCase(100,400,200,300,500);
+// [ 200, 300, 500] this is because 100 and 400 will be assigned to num1 and num2 respectively
+
+
+// Let's say we want to take object as an argument
+
+const user = {
+    username: "Leqdxr",
+    age: 20
+}
+
+function objectRelated(someObject) { // This way we can pass in any object we want
+    console.log(`Hello ${someObject.username}. Welcome to the app, you are ${someObject.age} years old`);
+}
+
+objectRelated(user); // Hello Leqdxr. Welcome to the app, you are 20 years old
+// We could pass in the object directly as well
+
+objectRelated({
+    username: "Aagaman",
+    age: 90
+}); // Hello Aagaman. Welcome to the app, you are 90 years old
+
+// We can also pass arrays similarly
+
+const myArray = [1,2,3,4,5,6];
+// This function will print 2nd value from an array
+function secondValueFromArray(someArray) {
+    console.log(someArray[1]);
+}
+secondValueFromArray(myArray); // 2
+secondValueFromArray([500,200,300,100]); // Or we could pass the value directly
+// 200
