910a555938c2a42a73798630aa6323b11d92f0b8
Aagaman Koirala <aagamankoirala10@gmail.com>
2026-08-23 16:37:48 +0545

    Updated Palindrome code to ignore whitespaces

--- Practice/palindrome.js
+++ Practice/palindrome.js
@@ -1,12 +1,13 @@
 // Write a program to check if the number is a palindrome
 
-const myString = "Racecar"
+const myString = "A man a plan a canal Panama"
 let reversedString = ""
 for(let i = myString.length-1; i>=0 ; i--) {
     reversedString = reversedString + myString[i]
 }
-if (reversedString.toLowerCase()==myString.toLowerCase()) { // .toLowerCase because Racecar would be reversed to racecaR, which would not be equal.
+if (reversedString.replaceAll(" ", "").toLowerCase()==myString.replaceAll(" ","").toLowerCase()) { // .toLowerCase because Racecar would be reversed to racecaR, which would not be equal.
     // so this converts all to lowercase
+    // replaceAll will replace all whitespaces with ""
     console.log(`${myString} is a palindrome`);
 }
 else {
